ca07d9ce89d489e335fce1d1826a94916fca2ffa
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-25 13:24:22 -0300

    adicionando links para paginas de cada categoria

--- src/Components/NaveBar.jsx
+++ src/Components/NaveBar.jsx
@@ -1,25 +1,27 @@
-import { React } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./NaveBar.css";
+import { Fragment, React } from "react";
+import { BrowserRouter as Routes, Route, Link } from "react-router-dom";
+import "../Styles/NaveBar.css";
 import Button from "@mui/material/Button";
-import SearchAppBar from './SearchBar';
-import Login from '../Pages/Login';
+import SearchAppBar from "./SearchBar";
 
 export default function NaveBar() {
   return (
-    <nav>
-      <div className="main_name">MovieFlix</div>
-      <div class="links">
-        <ul class="categories">
-          <li>Ação</li>
-          <li>Terror</li>
-          <li>Comédia</li>
-        </ul>
-      </div>
-      <div className="search">
-          <SearchAppBar/>
-      </div>
-      <Link to="Login"><Button
+    <Fragment>
+      <nav fullwidth>
+        <Link to='/' style={{ color: '#fffff'}}>
+          <div className="main_name">MovieFlix</div>
+        </Link>
+        <div class="links">
+          <ul class="categories">
+            <Link to='Movies_Action' style={{ color: "#fff", marginRight: '20%', textDecoration: 'none'}}><li>Ação</li></Link>
+            <Link to='Movies_Horror' style={{ color: '#fff', marginRight: '20%', textDecoration: 'none'}}><li>Terror</li></Link>
+            <Link to='Movies_Comedy' style={{ color: '#fff', marginRight: '20%', textDecoration: 'none'}}><li>Comédia</li></Link>
+          </ul>
+        </div>
+        <div className="search">
+          <SearchAppBar />
+        </div>
+        <Link to="Login"><Button
         className="login_button"
         style={{
           borderWidth: "3px",
@@ -37,6 +39,7 @@ export default function NaveBar() {
       <Routes>
         <Route path="Login" component={Login}></Route>
       </Routes>
-    </nav>
+      </nav>
+    </Fragment>
   );
 }
