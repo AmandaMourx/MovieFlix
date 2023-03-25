a7b96506656fbd52c1dc81cf50dc87cec35449d3
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-25 19:22:59 -0300

    arrumando as rotas

--- src/Pages/MeuPerfil.js
+++ src/Pages/MeuPerfil.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom' 
 
 export default function MeuPerfil() {
   return (
     <div>MeuPerfil
       <Link to="/"><button>Home</button></Link>
-      <Routes>
-        <Route path="/" component={App}></Route>
-      </Routes>
     </div>
   )
 }
