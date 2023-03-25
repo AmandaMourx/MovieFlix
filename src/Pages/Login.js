7a61ab0060b923685c703e550c03f78bf5ede21b
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-25 19:22:28 -0300

    arrumando as rotas

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Routes,Route,Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../Styles/Login.css'
-import Cadastro from './Cadastro';
+
 
 export default function Login() {
   return (
@@ -13,13 +13,11 @@ export default function Login() {
           <input className='login--password' type="password" placeholder='Senha'></input>
           <button className='login--enter'>Entrar</button>
             <h3>Ou</h3>
-          <Link to="/Cadastro"><button className='login--new' >Criar nova conta</button></Link>
-          <Routes>
-            <Route path="/Cadastro" component={Cadastro}></Route>
-          </Routes>
+          <Link to="/cadastro"><button className='login--new' >Criar nova conta</button></Link>
       </div>
 
     </div>
 
   )
 }
+
