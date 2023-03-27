8c22c2127862e7e0319c85e3f10e3cbe9584cf65
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-27 00:11:24 -0300

    configurando MovieCard

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../Styles/MoviesCateg.css';
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+
+  const API_IMAGE ='https://image.tmdb.org/t/p/w300'
+
   return (
-    <div>MovieCard</div>
+    <div className='movie_card'>
+      <div className='all_info'>
+        <div className='poster'>
+          <img src={API_IMAGE + props.poster_path} alt='poster do filme'/>
+        </div>
+
+        <p className='title_movies'>{props.title}</p>
+      </div>
+    </div>
   )
 }
 
