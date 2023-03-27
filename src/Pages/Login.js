f11a3239ed201ea6e02c0e880f0b910f3e110848
DaniloCTM <42286412+DaniloCTM@users.noreply.github.com>
2023-03-26 21:08:13 -0300

    Arrumando css do login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -9,11 +9,11 @@ export default function Login() {
       
       <div className='login--container'>
           <h2>Entrar</h2>
-          <input type="text" placeholder='E-mail'></input>
-          <input className='login--password' type="password" placeholder='Senha'></input>
-          <button className='login--enter'>Entrar</button>
+          <input className='login--input' type="text" placeholder='E-mail'></input>
+          <input className='login--input login--password' type="password" placeholder='Senha'></input>
+          <button className='login--button login--enter'>Entrar</button>
             <h3>Ou</h3>
-          <Link to="/cadastro"><button className='login--new' >Criar nova conta</button></Link>
+          <Link to="/cadastro"><button className='login--button login--new' >Criar nova conta</button></Link>
       </div>
 
     </div>
