83afbf64fe11c721ab51f5aff3d349b6cdfded53
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-27 16:38:07 -0300

    adicionando uma nova div para ter um css melhor

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -4,10 +4,10 @@ import home_image from '../images/home_image.png'
 
 export default function Hero() {
   return (
-    <>
+    <div className='hero-section'>
         <div className='title'>MovieFlix</div>
         <div className='subtitle'>A plataforma para organizar </div>
         <div className='subtitle2'> sua vida de cinéfilo. <img fullwidth fullheight style={{ marginLeft: '30%'}} src={home_image} alt='MovieFlix'className='home_image'/></div>
-    </>
+    </div>
   )
 }
