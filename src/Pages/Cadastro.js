71b38dfc5b99848fef10fec607c757e3e8533ec5
Otavio4283 <110172609+Otavio4283@users.noreply.github.com>
2023-03-29 09:27:43 -0300

    Update Cadastro.js
    Register funcionando

--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../Styles/Login.css'
 import '../Styles/Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-export default function Cadastro() {
+const Cadastro = (props) => {
+  
+  const [email,setEmail] = useState("");
+  const [usuario,setUsuario] = useState("");
+  const [senha,setSenha] = useState("");
+  const navigate =useNavigate();
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log(email);
+    navigate("/login");
+  };
+
   return (
     <div className='login--page'>
     <div className='login--container'>
         <h2>Criar conta</h2>
-        <input className='login--input' type="text" placeholder='E-mail'></input>
-        <input className='login--input' type="text" placeholder='Nome de usuário'></input>
-        <input className='login--input' type="password" placeholder='Senha'></input>
+        <form lassName="register-form" onSubmit={handleSignup}>
+        <input 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className='login--input' 
+        type="text" placeholder='E-mail' 
+        id="email" email="email" 
+        ></input>
+        
+        <input
+         value={usuario}
+         onChange={(e) => setUsuario(e.target.value)}
+         className='login--input' 
+         type="text" placeholder='Nome de usuário' 
+         id="usuario" usuario="usuario" 
+         ></input>
+
+        <input 
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
+        className='login--input' 
+        type="password" placeholder='Senha' 
+        id="senha" senha="senha" 
+        ></input>
+
         <input className='login--input' type="password" placeholder='Confirme sua senha'></input>
-        <Link to="/meu-perfil"><button className='login--button'>Criar conta</button></Link>
+      </form>
+        <Link to="/login"><button className='login--button'>Criar conta</button></Link>
     </div>
   </div>
   )
 }
 
+export default Cadastro;
