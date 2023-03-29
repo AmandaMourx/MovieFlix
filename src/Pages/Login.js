e27170a55a3c86d16db04c168d6843d908c1fe55
Otavio4283 <110172609+Otavio4283@users.noreply.github.com>
2023-03-29 09:28:41 -0300

    Update Login.js
    Login teste

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React,  {useState} from 'react'
 import { Link } from 'react-router-dom'
 import '../Styles/Login.css'
 
 
-export default function Login() {
+const Login =  (props) => {
+  
+  const [email,setEmail] = useState("");
+  const [senha,setSenha] = useState("");
+  
+  const submit = (e) =>{
+      e.preventDefault();
+      console.log(email);
+  }
   return (
     <div className='login--page'>
       
       <div className='login--container'>
           <h2>Entrar</h2>
-          <input className='login--input' type="text" placeholder='E-mail'></input>
-          <input className='login--input login--password' type="password" placeholder='Senha'></input>
-          <button className='login--button login--enter'>Entrar</button>
+          <form onSubmit={submit}>
+          <input value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='login--input' 
+            type="text" placeholder='E-mail' 
+            id="email" email="email" ></input>
+          <input 
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            className='login--input' 
+            type="password" placeholder='Senha' 
+            id="senha" senha="senha"
+            ></input>
+          </form>
+          <button className='login--button login--enter' onClick={submit}>Entrar</button>
             <h3>Ou</h3>
           <Link to="/cadastro"><button className='login--button login--new' >Criar nova conta</button></Link>
       </div>
@@ -21,3 +41,4 @@ export default function Login() {
   )
 }
 
+export default Login;
