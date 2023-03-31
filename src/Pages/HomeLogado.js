e3bdf10d93afdbc6fed1c3efc06d7a74db5c9d5c
DaniloCTM <42286412+DaniloCTM@users.noreply.github.com>
2023-03-30 21:54:00 -0300

    Informações do usuário

---
+++ src/Pages/HomeLogado.js
@@ -0,0 +1,40 @@
+import '../Styles/App.css';
+import Tmdb from '../Tmdb';
+import React, {useEffect, useState} from 'react';
+import MovieRow from '../Components/movieRow';
+import NavBarLogado from '../Components/NavBarLogado';
+import Hero from '../Components/HeroSection';
+import '../Styles/HeroSection.css'
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  
+  const [movieList, setMovieList] = useState([]);
+
+  // Quando a pagina é carregada o codigo dentro do useEffect é executado
+  useEffect( () => {
+    const loadAll = async () => {
+      //pegando a lista de filmes
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+      console.log(list)
+    }
+
+    loadAll();
+  }, []);
+  
+  return (
+    <div className='home-page'>
+      <NavBarLogado fullwidth/>
+      <Outlet/>
+      <Hero/>
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default App;
--- src/Pages/MeuPerfil.js
+++ src/Pages/MeuPerfil.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom' 
+import '../Styles/Perfil.css'
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export default function MeuPerfil() {
   return (
-    <div>MeuPerfil
-      <Link to="/"><button>Home</button></Link>
+    <div className='perfil--page'>
+      <div className='perfil--container'>
+        <div className='perfil--left'>
+          <KeyboardBackspaceIcon style={{marginLeft: '-10rem', fontSize:'2rem'}}/><br></br>
+          <AccountCircleIcon className='icon-cont' style={{fontSize: '12rem'}}/>  
+          <h3>Minhas informações</h3>
+        </div>  
+        <div className='perfil--right'>
+          <p className='perfil--title'>Visão geral</p>
+          <div className='perfil--data'>
+            <p className='perfil--text'>Nome de usuário</p>
+            <p className='perfil--text'>Nome</p>
+          </div>
+          <div className='perfil--data'>
+            <p className='perfil--text'>Endereço de e-mail</p>
+            <p className='perfil--text'>E-mail</p>
+          </div>
+          <div className='perfil--data'>
+            <p className='perfil--text'>Senha</p>
+            <p className='perfil--text'>Senha</p>
+          </div>
+          
+          
+        </div>
+          
+      </div>
     </div>
   )
 }
