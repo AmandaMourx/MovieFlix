cb3fed3aafa68932aee18b9bdc6b922b2c42a244
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-31 20:44:25 -0300

    tentando fazer o user authentication
    -not working -

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -1,27 +1,75 @@
-import { APIKey } from "../config/Key";
-import React, {createContext, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const Autentificacao = createContext({});
+export const AuthContext = createContext({});
 
-export const RealizarAutentific = ({children}) =>{
-    const [infoUsuario,setInfousuario] = useState([]);
-    const [logged,setLogged] = useState(false);
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
 
-    function criarToken(){
+  useEffect(() => {
+    const userToken = localStorage.getItem("user_token");
+    const usersStorage = localStorage.getItem("users_bd");
 
+    if (userToken && usersStorage) {
+      const hasUser = JSON.parse(usersStorage)?.filter(
+        (user) => user.email === JSON.parse(userToken).email
+      );
+
+      if (hasUser) setUser(hasUser[0]);
     }
+  }, []);
 
-    function Sessao(){
+  const signin = (email, senha) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
 
+    const hasUser = usersStorage?.filter((user) => user.email === email);
+
+    if (hasUser?.length) {
+      if (hasUser[0].email === email && hasUser[0].senha === senha) {
+        const token = Math.random().toString(36).substring(2);
+        localStorage.setItem("user_token", JSON.stringify({ email, token }));
+        setUser({ email, senha });
+        return;
+      } else {
+        return "E-mail ou senha incorretos";
+      }
+    } else {
+      return "Usuário não cadastrado";
     }
+  };
 
-    function criarSessao(){
-
-    }
-
-    function getInfoUsuario(){
-
-    }
+  const signup = (email, senha) => {
     
-    return <Autentificacao.Provider value={{infoUsuario,logged,criarToken,Sessao,criarSessao}}>{children}</Autentificacao.Provider>
-}
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
+
+    const hasUser = usersStorage?.filter((user) => user.email === email);
+
+    if (hasUser?.length) {
+      return "Já tem uma conta com esse E-mail";
+    }
+
+    let newUser;
+
+    if (usersStorage) {
+      newUser = [...usersStorage, { email, senha }];
+    } else {
+      newUser = [{ email, senha }];
+    }
+
+    localStorage.setItem("users_bd", JSON.stringify(newUser));
+
+    return;
+  };
+
+  const signout = () => {
+    setUser(null);
+    localStorage.removeItem("user_token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signed: !!user, signin, signup, signout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
