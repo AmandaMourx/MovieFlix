897f2b07985c3660962a7a6da2417bd1026d1e4e
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-31 20:45:13 -0300

    updating importations

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
-import useContext from "react";
-import Autentificacao from "../contexts/Auth";
+import { useContext } from "react";
+import { AuthContext } from "../context/Auth";
 
 const useAuth = () =>{
     const contexto = useContext(Autentificacao);
