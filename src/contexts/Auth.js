b0082b961ea11da6b58b53d3c5760be3ae1a64cf
Otavio4283 <110172609+Otavio4283@users.noreply.github.com>
2023-03-31 08:33:04 -0300

    Update Auth.js

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -1,3 +1,27 @@
 import { APIKey } from "../config/Key";
 import React, {createContext, useState} from "react";
 
+export const Autentificacao = createContext({});
+
+export const RealizarAutentific = ({children}) =>{
+    const [infoUsuario,setInfousuario] = useState([]);
+    const [logged,setLogged] = useState(false);
+
+    function criarToken(){
+
+    }
+
+    function Sessao(){
+
+    }
+
+    function criarSessao(){
+
+    }
+
+    function getInfoUsuario(){
+
+    }
+    
+    return <Autentificacao.Provider value={{infoUsuario,logged,criarToken,Sessao,criarSessao}}>{children}</Autentificacao.Provider>
+}
