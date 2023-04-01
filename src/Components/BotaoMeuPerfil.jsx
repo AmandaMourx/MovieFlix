9d2caed55b689b1b805746ccd9d44a665c485b67
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-31 23:03:28 -0300

    Botao para ir para o meu perfil

---
+++ src/Components/BotaoMeuPerfil.jsx
@@ -0,0 +1,39 @@
+import { Button } from "@mui/material";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { useNavigate } from "react-router-dom";
+
+export default function BotaoMeuPerfil() {
+  const navigate = useNavigate();
+
+  const changeUrlMeuPerfil = () => {
+    navigate('/meu-perfil');
+  };
+
+  return (
+    <>
+      <Button
+        style={{
+          borderWidth: "3px",
+          borderColor: "white",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "10px",
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: "7%",
+          marginRight: "0%",
+          textDecoration: "none",
+          marginTop: "1%",
+          marginBottom: "1%",
+          padding: "10px",
+          textAlign: 'center',
+        }}
+        variant="outlined"
+        onClick={changeUrlMeuPerfil}
+      >
+        {" "}
+        MEU PERFIL{" "}
+      </Button>
+    </>
+  );
+}
