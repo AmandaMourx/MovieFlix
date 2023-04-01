82ca6512d01c8e1d61fe56fb757b19e8c6100765
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-03-31 21:18:50 -0300

    updating trying to use the auth

--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -1,13 +1,40 @@
-import React, {useState} from 'react'
-import '../Styles/Login.css'
-import '../Styles/Login.css'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import '../Styles/Login.css';
+import { Link, /*useNavigate*/ } from 'react-router-dom';
+//import useAuth from "../hooks/useAuth";
 
 const Cadastro = (props) => {
+  
+  /*
+    const navigate = useNavigate();
+
+    const {signup} = useAuth();
+
+    const handleSignup = () => {
+      if(!email | !emailConf | !senha){
+        setError("Preencha todos os campos");
+        return;
+      }else if (email !== emailConf){
+        setError("Os e-mails não são iguais");
+        return;
+      }
+
+      const res = signup(email, senha);
+
+      if(res){
+        setError(res);
+        return;
+      }
+
+      alert("Usuário cadastrado com sucesso!");
+      navigate('/login');
+    };
+  */
   
   const [email,setEmail] = useState("");
   const [usuario,setUsuario] = useState("");
   const [senha,setSenha] = useState("");
+  
   const handleSignup = (e) => {
     e.preventDefault();
     console.log(email);
@@ -22,7 +49,7 @@ const Cadastro = (props) => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         className='login--input' 
-        type="text" placeholder='E-mail' 
+        type="email" placeholder='Digite seu E-mail'  
         id="email" email="email" 
         ></input>
         
@@ -38,13 +65,22 @@ const Cadastro = (props) => {
         value={senha}
         onChange={(e) => setSenha(e.target.value)}
         className='login--input' 
-        type="password" placeholder='Senha' 
+        type="password" placeholder='Digite sua senha' 
         id="senha" senha="senha" 
         ></input>
-
         <input className='login--input' type="password" placeholder='Confirme sua senha'></input>
       </form>
         <Link to="/login"><button className='login--button'>Criar conta</button></Link>
+          <div className="divider">
+            <hr />
+              <span> </span>
+              <h1>Já tem uma conta?</h1>
+              <span> </span>
+            <hr />
+        </div>
+        <Link to="/login">
+          <button className="login--button login--new">Entrar</button>
+        </Link>
     </div>
   </div>
   )
